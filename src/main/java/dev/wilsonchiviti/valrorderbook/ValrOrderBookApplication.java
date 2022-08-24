8125891d3514e0b6bf9c005b4897100875543ecd
wilson <wilsonchiviti@gmail.com>
2022-08-24 21:21:15 +0200

    first commit

---
+++ src/main/java/dev/wilsonchiviti/valrorderbook/ValrOrderBookApplication.java
@@ -0,0 +1,13 @@
+package dev.wilsonchiviti.valrorderbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValrOrderBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValrOrderBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/wilsonchiviti/valrorderbook/ValrOrderBookApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.wilsonchiviti.valrorderbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValrOrderBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
